87dc62a2fb5c7e0c0c72e86e08b8c170b3f879b5
blowuppp <mark@mgv.co.uk>
2024-12-26 16:05:33 +0000

    update for vis editor

---
+++ stackbit.config.js
@@ -0,0 +1,26 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  ssgName: "nextjs",
+  nodeVersion: "18",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.md",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "content", type: "markdown", required: true }
+          ]
+        }
+      ],
+    }),
+  ],
+});
